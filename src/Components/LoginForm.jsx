1d116d1d006eabca20337033a02ffebb330838c7
Joseph-Carter <1joseph1carter@gmail.com>
2023-11-22 21:09:55 -0500

    created both login and sign up forms with proper routing, working on rendering and getting posts information.

---
+++ src/Components/LoginForm.jsx
@@ -0,0 +1,61 @@
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useAuth } from "./UserAuth/UserContext";
+
+const API = import.meta.env.VITE_API_URL;
+
+const LoginForm = () => {
+  const navigate = useNavigate();
+  const [userInput, setUserInput] = useState({
+    email: "",
+    password: "",
+  });
+  const { user, loginUser } = useAuth();
+  useEffect(() => {
+    if (user.id) {
+      navigate(`/users/${user.id}/posts`);
+    }
+  }, [user]);
+
+  const handleTextInput = (e) => {
+    setUserInput({ ...userInput, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    loginUser(userInput);
+  };
+
+  return (
+    <div className="loginForm">
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">Email:</label>
+        <input
+          id="email"
+          value={userInput.email}
+          type="text"
+          onChange={handleTextInput}
+          placeholder="email"
+          required
+        />
+        <label htmlFor="password">Password:</label>
+        <input
+          id="password"
+          value={userInput.password}
+          type="password"
+          onChange={handleTextInput}
+          placeholder="password"
+          required
+        />
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+      <br />
+      <button type="button">
+        <Link to={`/signup`}>Sign Up</Link>
+      </button>
+    </div>
+  );
+};
+
+export default LoginForm;
---
+++ src/Components/SignUpForm.jsx
@@ -0,0 +1,93 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const API = import.meta.env.VITE_API_URL;
+
+const SignUpForm = () => {
+  const navigate = useNavigate();
+  const [user, setUser] = useState({
+    email: "",
+    firstName: "",
+    lastName: "",
+    password: "",
+    passwordConfirmation: "",
+  });
+
+  const addUser = () => {
+    fetch(`${API}/users/signup`, {
+      method: "POST",
+      body: JSON.stringify(user),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then(() => {
+        navigate(`/`);
+      })
+      .catch((error) => console.error("catch", error));
+  };
+
+  const handleTextInput = (e) => {
+    setUserInput({ ...userInput, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    loginUser(userInput);
+  };
+
+  return (
+    <div className="signUpForm">
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="firstName">First Name:</label>
+        <input
+          id="firstName"
+          value={user.firstName}
+          type="text"
+          onChange={handleTextInput}
+          placeholder="First Name"
+          required
+        />
+        <label htmlFor="lastName">Last Name:</label>
+        <input
+          id="lastName"
+          value={user.lastName}
+          type="text"
+          onChange={handleTextInput}
+          placeholder="Last Name"
+          required
+        />
+        <label htmlFor="email">Email:</label>
+        <input
+          id="email"
+          value={user.email}
+          type="text"
+          onChange={handleTextInput}
+          placeholder="email"
+          required
+         />
+         <label htmlFor="password">Password:</label>
+         <input
+          id="password"
+          value={user.password}
+          type="password"
+          onChange={handleTextInput}
+          placeholder="password"
+          required
+         />
+         <label htmlFor="passwordConfirmation">Confirm Password:</label>
+         <input
+          id="passwordConfirmation"
+          value={user.passwordConfirmation}
+          type="password"
+          onChange={handleTextInput}
+          placeholder="password"
+          required
+         />
+         <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default SignUpForm;
